3b8f8052add659e81f07e825e1603403b047a3b7
tandasat <tanda.sat@gmail.com>
2016-04-11 21:44:52 -0700

    support usage from C++ files

--- cs_driver/cs_driver_mm.h
+++ cs_driver/cs_driver_mm.h
@@ -1,6 +1,10 @@
 #ifndef CAPSTONE_CS_DRIVER_MM_H_
 #define CAPSTONE_CS_DRIVER_MM_H_
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include <capstone.h>
 
 /*
@@ -12,5 +16,9 @@
  NOTE: cs_driver_init() can be called at IRQL <= DISPATCH_LEVEL.
 */
 cs_err CAPSTONE_API cs_driver_mm_init();
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif  // CAPSTONE_CS_DRIVER_MM_H_
